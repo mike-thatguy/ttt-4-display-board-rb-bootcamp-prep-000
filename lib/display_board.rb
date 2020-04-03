6696305b3e2f14c3cc318a5f5f13894f0d39d8c5
Mike Gretton <mikegretton@gmail.com>
2020-04-03 01:30:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 def display_board(arr)
   puts " #{arr[0]} | #{arr[1]} | #{arr[2]} "
-  puts "---------"
+  puts "-----------"
   puts " #{arr[3]} | #{arr[4]} | #{arr[5]} "
-  puts "---------"
+  puts "-----------"
   puts " #{arr[6]} | #{arr[7]} | #{arr[8]} "
 end
   
